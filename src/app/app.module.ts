f0f79721dff641b4db2cb5408db3e3cbb22f6d44
GunPKate <gun@builk.com>
2022-10-02 19:18:58 +0700

    add component (ng g c `${c name}`)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { EditComponent } from './edit/edit.component';
+import { NewComponent } from './new/new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    EditComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule,
